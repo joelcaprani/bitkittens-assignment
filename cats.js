9cfaa470ca904cace9cf64c4f0242e3bdc9fca7f
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-02-20 12:45:41 -0500

    assignment done

---
+++ cats.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $('.summon-cats').click(function() {
+
+        $.ajax({
+            url: 'http://bitkittens.herokuapp.com/cats.json',
+         method: 'GET',
+       datatype: 'JSON'
+   }).done(function (data) {
+
+
+      for(var i=0; i < data.cats.length; i++) {
+        $('<img>')
+         .attr('src', data.cats[i]['photo'])
+         .attr('alt', 'Photo of: ' + data.cats[i]['name'])
+         .appendTo('#cat' + (i + 1) )
+      ;}
+    });
+  });
+ });
